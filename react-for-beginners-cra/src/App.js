a57c0f6bdfc3393f3a494156e1063ab66a98641c
이영민 <dldudals0728@naver.com>
2022-07-09 18:47:08 +0900

    #6.1 useEffect

--- react-for-beginners-cra/src/App.js
+++ react-for-beginners-cra/src/App.js
@@ -1,11 +1,17 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("i run all the time.");
+  // useEfact(): 첫번째 인자로 전달되는 함수를 "첫번째 랜더링 에서만" 작동하도록 해준다. (like call the api)
+  useEffect(() => {
+    console.log("CALL THE API...");
+  }, []);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text="Continue" />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
