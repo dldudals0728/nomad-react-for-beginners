b6cb7ba77cd34eb78a69609205da8f65ea86cd40
이영민 <dldudals0728@naver.com>
2022-07-09 19:09:58 +0900

    #6.2 Deps inspect -> console창에 log가 두번씩 나오는 것을 없애기 위해 index.js 파일의 <React.StrictMode></React.StrictMode> 삭제

--- react-for-beginners-cra/src/App.js
+++ react-for-beginners-cra/src/App.js
@@ -2,14 +2,30 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("i run all the time.");
-  // useEfact(): 첫번째 인자로 전달되는 함수를 "첫번째 랜더링 에서만" 작동하도록 해준다. (like call the api)
+  const onChange = (event) => setKeyword(event.target.value);
   useEffect(() => {
-    console.log("CALL THE API...");
+    console.log("I run only once.");
   }, []);
+  // useEffect()의 두번째 인자로 들어가는 배열의 요소를 observe한다. 해당 요소의 변화가 있을 때 첫번째 인자의 함수를 실행시킨다.
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+  useEffect(() => {
+    console.log("I run when 'keyword' or 'counter' changes");
+  }, [keyword, counter]);
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
