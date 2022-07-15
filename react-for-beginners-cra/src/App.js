46d875bb174279e8925ef1d78c7d9b2118b4b1c1
devid <dldudals0728@naver.com>
2022-07-15 16:16:42 +0900

    #7.4 Movie App part One / part Two finish add folder components, routes npm install react-router-dom

--- react-for-beginners-cra/src/App.js
+++ react-for-beginners-cra/src/App.js
@@ -1,54 +1,6 @@
-import { useEffect, useState } from "react";
-// api: api.coinpaprika.com/v1/tickers
+// App.js 는 더 이상 화면을 보여주지 않고, router(page, home, ... URL을 보고있는 Component)를 render한다.
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [dollers, setDollers] = useState(0);
-  const [selectedCoin, setSelectedCoin] = useState(0);
-  const onChange = (event) => setDollers(event.target.value);
-  const btcChange = (event) => {
-    console.log(event.target.selectedIndex);
-    setSelectedCoin(parseInt(event.target.selectedIndex));
-  };
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
-  }, []);
-  return (
-    <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
-      {loading ? (
-        <strong>Loading...</strong>
-      ) : (
-        <div>
-          input your dollers!
-          <input
-            onChange={onChange}
-            value={dollers || ""}
-            type="number"
-            placeholder="$$$$"
-          />
-          <br />
-          <select onChange={btcChange}>
-            {coins.map((coin) => (
-              <option key={coin.id}>
-                {coin.name} ({coin.symbol}): {coin.quotes.USD.price} BTC
-              </option>
-            ))}
-          </select>
-          <br />
-          <strong>
-            you can buy {coins[selectedCoin].name}:{" "}
-            {dollers / coins[selectedCoin].quotes.USD.price}BTC
-          </strong>
-        </div>
-      )}
-    </div>
-  );
+  return null;
 }
 
 export default App;
---
+++ react-for-beginners-cra/src/routes/Home.js
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+import Movie from "../components/Movie";
+
+function Home() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  // fetch().then().then()... 을 사용하지 않고 async-await을 사용한다. => then보다 async-await 추천!!!
+  const getMovies = async () => {
+    /*
+      const response와 const json을 하나로 묶을 수도 있다.
+      const json = await (
+        await fetch(
+          `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+        )
+      ).json();
+      */
+    const response = await fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+    );
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+  console.log(movies);
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {/** map을 사용할 때 마다 key값으로 unique한 값을 주어야 한다! */}
+          {movies.map((movie) => (
+            <Movie
+              key={movie.id}
+              coverImg={movie.medium_cover_image}
+              title={movie.title}
+              summary={movie.summary}
+              genres={movie.genres}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Home;
