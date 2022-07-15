47b932851e74e1add2c396ff6af8e53ebfe1f2eb
devid <dldudals0728@naver.com>
2022-07-15 17:31:34 +0900

    #7.5 React Router using react-router-dom v6

--- react-for-beginners-cra/src/App.js
+++ react-for-beginners-cra/src/App.js
@@ -1,6 +1,22 @@
-// App.js 는 더 이상 화면을 보여주지 않고, router(page, home, ... URL을 보고있는 Component)를 render한다.
+// react-router-dom v6 이상부터 Switch -> Routes로 변경
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 function App() {
-  return null;
+  return (
+    <Router>
+      {/** Switch: Route(URL)을 찾는다. Route를 찾으면 component를 rendering한다. */}
+      <Routes>
+        {/* 아래 코드로 변경
+        <Route path="/">
+          <Home />
+        </Route> */}
+        <Route path="/" element={<Home />} />
+        <Route path="/movie" element={<Detail />} />
+        <Route path="/hello" element={<h1>Hello</h1>} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
